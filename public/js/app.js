beb43841a58dbe83c4314d37ca7051fee6afff5d
Rachel Yankelevitz <rachel.yankelevitz@bluecore.com>
2017-05-23 20:06:55 -0400

    this is the final project

--- public/js/app.js
+++ public/js/app.js
@@ -4,30 +4,25 @@ $(document).ready(function() {
     // var clientId = "5M-C08bDvKu382tlHqxq3Q"
     // var PROXY_URL = "https://accesscontrolalloworiginall.herokuapp.com/";
     //bind events and stuff
-    var rest_tmpl = '\
-        <article>\
-            <h3><%= name %></h3>\
-            <div>\
-                <%=location.address1 %>, <%=location.city %>, <%=location.state %>, <%=location.zip_code %>\
-            </div>\
-            <div>\
-                Phone: <%= display_phone %>\
-            </div>\
-            <div>\
-                Price: <%= price %>\
-            </div>\
-            <div>\
-                URL: <a href="<%-url%>" target="_blank">\
-                <%=name %>\
-            </div>\
-            <hr/>\
-        </article>';
+
 
     var App = {
         init: function() {
             App.bindEvents();
         },
+        setView: function(viewType) {
+          if (viewType === 'loader') {
+            $('.loader').removeClass('hidden');
+            $('.loader').addClass('loading');
+            $('.loader').siblings().addClass('hidden');
+          } else if (viewType === 'page') {
+            $('.loader').addClass('hidden');
+            $('.loader').removeClass('loading');
+            $('.loader').siblings().removeClass('hidden');
+          }
+        },
         requestFromYelp: function(zip) {
+            App.setView('loader');
           var request = $.ajax('/yelp', {
               dataType: 'json',
               data: {
@@ -36,6 +31,7 @@ $(document).ready(function() {
           });
 
           request.done(function(response) {
+            App.setView('page');
               App.renderRestaurants(response);
           });
         },
@@ -46,21 +42,81 @@ $(document).ready(function() {
                 App.requestFromYelp(zipInput);
                 //also show processing modal
             });
+
+            //open modal onclick of "more button"//
+            $('#padding').on("click", ".readMore", function() {
+                var index = $('.readMore').index($(this));
+                var restaurant = App.restaurantList[index];
+                var yelpUrl = restaurant.url
+                var newLink = $('<a/>', {
+                    id : "id5",
+                    name : "link",
+                    href : yelpUrl,
+                    text : "Yelp Page",
+                    target: '_blank'
+                });
+                $('#linkHold').append(newLink);
+
+            $('#popup_name').html(restaurant.name);
+            $('#popup_phone').html(restaurant.display_phone);
+            $('#popup_address').html(restaurant.location.address);
+            $('#popup_city').html(restaurant.location.city);
+            $('#popup_state').html(restaurant.location.state);
+            $('#popup_zip').html(restaurant.location.zip_code);                // $('#popup_review').html(restaurant.rating);
+
+
+
+            // var yelpUrl = $(this).attr('href');
+            //  $('#popUp', 'a').attr('href', yelpUrl);
+
+
+
+            $('#popUp').modal();
+                console.log("this is a test!", index)
+            });
+// rendering modal
+
+            //  $('.zipbutton').on("click", function() {
+            //     $(".loading").show("#loader").modal();
+            // });
+
         },
+        restaurantList: [],
         //add the results to the footer div!!!
         renderRestaurants: function(response) {
-            var restaurantList = response.businesses;
+            App.restaurantList = response.businesses;
 
-            var makeHtml = _.template(rest_tmpl);
+            var makeHtml = _.template(App.rest_tmpl);
 
-            restaurantList.forEach(function(restaurant){
+            App.restaurantList.forEach(function(restaurant){
                 var html = makeHtml(restaurant);
-                $('section#padding').append(html);
+                $('div#padding').append(html);
             });
-            console.log(restaurantList);
-            // $(".footer").html(minTempMarkup + maxTempMarkup);
-        }
+            console.log(App.restaurantList);
+
+        },
+        rest_tmpl: '\
+        <article class="article">\
+            <h3><%= name %></h3>\
+            <div>\
+                Address: <%=location.address1 %>, <%=location.city %>, <%=location.state %>, <%=location.zip_code %>\
+            </div>\
+            <div>\
+                Phone: <%= display_phone %>\
+            </div>\
+            <div>\
+                Price: <%= price %>\
+            </div>\
+            <div>\
+                <button class="readMore">More Info</button>\
+            </div>\
+            <section class="featuredImage">\
+            <img src="<%= image_url %>" alt="">\
+            </section>\
+            <hr/>\
+        </article>'
     };
 //this is in case I've screwed up//
     App.init();
+    // window.App = App;
 });
